a7f6bf8779ac10d3cdbc6485af39dfec3084feaa
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2025-08-07 07:11:40 -0300

    fix:again

--- repositories/casosRepository.js
+++ repositories/casosRepository.js
@@ -1,15 +1,4 @@
-const express = require("express");
 const db = require("../db/db");
-
-/*
-   {
-        id: "f5fb2ad5-22a8-4cb4-90f2-8733517a0d46",
-        titulo: "homicidio",
-        descricao: "Disparos foram reportados às 22:33 do dia 10/07/2007 na região do bairro União, resultando na morte da vítima, um homem de 45 anos.",
-        status: "aberto",
-        agente_id: "401bccf5-cf9e-489d-8412-446cd169a0f1" 
-    
-    }*/
 
 async function getAll({ agente_id, status } = {}) {
   try {
@@ -79,7 +68,7 @@ async function update(id, fieldsToUpdate) {
     const updated = await db("casos")
       .where({ id: id })
       .update(fieldsToUpdate, ["*"]);
-    if (!updateAgente || updateAgente.length === 0) {
+    if (!updated || updated.length === 0) {
       return false;
     }
     return updated[0];
