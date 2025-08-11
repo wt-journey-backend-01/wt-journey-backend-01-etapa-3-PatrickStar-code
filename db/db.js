1c163fb3feed96f641923a61cf83018dda3d11b7
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2025-08-11 13:13:48 -0300

    fix:date format

--- db/db.js
+++ db/db.js
@@ -1,5 +1,8 @@
 const knexConfig = require("../knexfile");
 const knex = require("knex");
+const { types } = require("pg");
+
+types.setTypeParser(1082, (val) => val);
 
 const nodeEnv = process.env.NODE_ENV || "development";
 const config = knexConfig[nodeEnv];
